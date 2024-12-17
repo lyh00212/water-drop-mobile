d0a5a2c6dab799673bfb80741a6fd6696a4cf8fc
lyh00212 <1149248201@qq.com>
2024-12-17 22:30:38 +0800

    【功能修改】完善头像上传至阿里云oss服务功能

--- src/graphql/oss.ts
+++ src/graphql/oss.ts
@@ -3,10 +3,12 @@ import { gql } from '@apollo/client'
 export const GET_OSS_INFO = gql`
     query getOSSInfo {
         getOSSInfo {
-            expire
-            policy
             signature
             accessId
+            expire
+            policy
+            host
+            dir
         }
     }
 `
